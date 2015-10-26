9a24e6adb4323c28111dd07e834cc51b0f03c194
Prod OSQA Server <tech@bebraven.org>
2015-10-26 13:21:06 -0400

    Initial customization/configuration, including merging in the Bitnami changes

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ import sys
 
 SITE_ID = 1
 
-SECRET_KEY = 'a;::qCl1mfh?avagttOJ;8f5Rr54d,9qy7;o15M2cYO75?OQo51u3LnQ;!8N.:,7'
+# The actual value of this is set in settings_local.py to protect it
+#SECRET_KEY = 'a;::qCl1mfh?avagttOJ;8f5Rr54d,9qy7;o15M2cYO75?OQo51u3LnQ;!8N.:,7'
 
 CACHE_MAX_KEY_LENGTH = 235
 
